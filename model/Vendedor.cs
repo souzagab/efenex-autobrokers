66634bffa756f0dfef1358c77b78b1f8dc69c6d4
Gabriel de Souza <desouza.gabriel@icloud.com>
2020-10-13 22:27:54 -0300

    Adiciona model de vendas

---
+++ model/Vendedor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Efenex.model
+{
+    class Vendedor : BaseModel
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ model/venda/Venda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+
+namespace Efenex.model.venda
+{
+    // Facade
+    class Venda : BaseModel
+    {
+        private readonly IVeiculo veiculo;
+        private readonly IVendedor vendedor;
+        public string DataVenda = DateTime.Today.ToString();
+        public long veiculo_id { get; set; }
+        public long vendedor_id { get; set; }
+        public long cliente_id { get; set; }
+
+        public bool Save()
+        {
+            var response = Connection.Execute(@$"INSERT INTO {TableName()}(DataVenda,vendedor_id,veiculo_id,cliente_id) VALUES(@Data,@vendedor,@veiculo,@cliente)", this);
+            if (response == 1)
+            {
+                veiculo.MarcarComoVendido(this.veiculo_id);
+                vendedor.RealizarVenda();
+                return true;
+            }
+            return false;
+        }
+    }
+}
